475e43ff0a356846cba68ff28b42bd29c6cb7e88
blade <blade@a95a6be8-091b-0410-adaf-d31e6857962f>
2006-09-05 18:52:54 +0000

    Changed cam check a bit to match the one reported by Petr Salinger as "working"
    git-svn-id: svn://svn.debian.org/debburn/nonameyet/trunk@222 a95a6be8-091b-0410-adaf-d31e6857962f

--- include/AddScgBits.cmake
+++ include/AddScgBits.cmake
@@ -1,14 +1,17 @@
 
 LIST(APPEND SCG_LIBS "scg")
-INCLUDE(CheckIncludeFile)
-CHECK_INCLUDE_FILE("camlib.h" HAVE_CAMLIB_H)
+
+INCLUDE(CheckIncludeFiles)
+CHECK_INCLUDE_FILES("stdio.h;camlib.h" HAVE_CAMLIB_H)
+
 if(HAVE_CAMLIB_H)
 
-   # quick an dirty, should better be a var used by libscg only, analogous to
-   # SCG_SELF_LIBS
+   # quick an dirty, should better become a variable used by libscg only,
+   # analogous to SCG_SELF_LIBS
    ADD_DEFINITIONS(-DHAVE_CAMLIB_H)
 
    LIST(APPEND SCG_LIBS "cam")
    LIST(APPEND SCG_SELF_LIBS "cam")
+
 endif(HAVE_CAMLIB_H)
 
