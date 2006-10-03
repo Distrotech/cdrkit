749c503f1ef7a8f5deeadeff5858fb02e71e72fb
blade <blade@a95a6be8-091b-0410-adaf-d31e6857962f>
2006-10-03 12:24:08 +0000

    SIZE_T_MAX guard
    git-svn-id: svn://svn.debian.org/debburn/nonameyet/trunk@356 a95a6be8-091b-0410-adaf-d31e6857962f

--- include/utypes.h
+++ include/utypes.h
@@ -382,7 +382,9 @@ typedef	unsigned char		UInt8_t;
 #endif
 
 #define	SIZE_T_MIN	TYPE_MINVAL(size_t)
+#ifndef SIZE_T_MAX
 #define	SIZE_T_MAX	TYPE_MAXVAL(size_t)
+#endif
 
 #define	SSIZE_T_MIN	TYPE_MINVAL(ssize_t)
 #define	SSIZE_T_MAX	TYPE_MAXVAL(ssize_t)
