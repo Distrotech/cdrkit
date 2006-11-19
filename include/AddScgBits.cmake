702dce3fbeb57fd4d5a7b7884055db2a6dd02efb
blade <blade@a95a6be8-091b-0410-adaf-d31e6857962f>
2006-11-19 17:41:44 +0000

    Removal of the most offensive parts
    git-svn-id: svn://svn.debian.org/debburn/nonameyet/branches/surgery@401 a95a6be8-091b-0410-adaf-d31e6857962f

--- include/AddScgBits.cmake
+++ include/AddScgBits.cmake
@@ -1,17 +1,18 @@
 
-LIST(APPEND EXTRA_LIBS "scg")
-
-INCLUDE(CheckIncludeFiles)
-CHECK_INCLUDE_FILES("stdio.h;camlib.h" HAVE_CAMLIB_H)
-
-if(HAVE_CAMLIB_H)
-
-   # quick an dirty, should better become a variable used by libscg only,
-   # analogous to SCG_SELF_LIBS
-   ADD_DEFINITIONS(-DHAVE_CAMLIB_H)
-
-   LIST(APPEND EXTRA_LIBS "cam")
-   LIST(APPEND SCG_SELF_LIBS "cam")
-
-endif(HAVE_CAMLIB_H)
-
+#
+#LIST(APPEND EXTRA_LIBS "scg")
+#
+#INCLUDE(CheckIncludeFiles)
+#CHECK_INCLUDE_FILES("stdio.h;camlib.h" HAVE_CAMLIB_H)
+#
+#if(HAVE_CAMLIB_H)
+#
+#   # quick an dirty, should better become a variable used by libscg only,
+#   # analogous to SCG_SELF_LIBS
+#   ADD_DEFINITIONS(-DHAVE_CAMLIB_H)
+#
+#   LIST(APPEND EXTRA_LIBS "cam")
+#   LIST(APPEND SCG_SELF_LIBS "cam")
+#
+#endif(HAVE_CAMLIB_H)
+#
