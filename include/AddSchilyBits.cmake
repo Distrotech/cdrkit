0f341dd35f4408d87bde2c5955b3e6d8934e7bef
blade <blade@a95a6be8-091b-0410-adaf-d31e6857962f>
2006-11-26 14:40:42 +0000

    Renamed the binary name of libschily to librols
    git-svn-id: svn://svn.debian.org/debburn/cdrkit/trunk@490 a95a6be8-091b-0410-adaf-d31e6857962f

--- include/AddSchilyBits.cmake
+++ include/AddSchilyBits.cmake
@@ -1,30 +1,7 @@
-IF(NOT CHECKED_SCHILY)
-   SET(CHECKED_SCHILY 1)
+IF(NOT CHECKED_rols)
+   SET(CHECKED_rols 1)
 
-   INCLUDE(CheckCSourceCompiles)
-   LIST(APPEND EXTRA_LIBS "schily")
+   LIST(APPEND EXTRA_LIBS "rols")
 
-
-   SET(TESTSRC " 
-   #include <math.h>
-   int main() { return isnan(1); }
-   ")
-
-   SET(CMAKE_REQUIRED_LIBRARIES )
-   CHECK_C_SOURCE_COMPILES("${TESTSRC}" HAVE_LIBC_ISNAN)
-
-   #//#define HAVE_ISNAN 1    /* isnan() is present in libc or libm */
-   # assuming that, see xconfig.h.in, until we meet an OS where is no
-   # native isnan(...) function available
-
-   IF(NOT HAVE_LIBC_ISNAN)
-      SET(CMAKE_REQUIRED_LIBRARIES m)
-      LIST(APPEND EXTRA_LIBS m)
-      CHECK_C_SOURCE_COMPILES("${TESTSRC}" HAVE_LIBM_ISNAN)
-      IF(NOT HAVE_LIBM_ISNAN)
-         MESSAGE(FATAL_ERROR "isnan function not found anywhere on ${CMAKE_SYSTEM_NAME}")
-      ENDIF(NOT HAVE_LIBM_ISNAN)
-   ENDIF(NOT HAVE_LIBC_ISNAN)
-
-ENDIF(NOT CHECKED_SCHILY)
+ENDIF(NOT CHECKED_rols)
 
