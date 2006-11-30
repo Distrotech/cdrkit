45f85c70cb6b5f7f9dd1cf29874f8c24f07b9315
blade <blade@a95a6be8-091b-0410-adaf-d31e6857962f>
2006-11-30 02:05:00 +0000

    Checks for extra additional network libs factored out, added something for libnls
    git-svn-id: svn://svn.debian.org/debburn/cdrkit/trunk@526 a95a6be8-091b-0410-adaf-d31e6857962f

---
+++ include/AddNetworkBits.cmake
@@ -0,0 +1,40 @@
+
+# various checks add additional of extra libs, most likely for SunOS
+
+INCLUDE(CheckCSourceCompiles)
+
+SET(TESTSRC "
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main(int argc, char **argv) {
+return socket(AF_INET, SOCK_STREAM, 0);
+}
+")
+
+SET(CMAKE_REQUIRED_LIBRARIES )
+CHECK_C_SOURCE_COMPILES("${TESTSRC}" LIBC_SOCKET)
+
+IF(NOT LIBC_SOCKET)
+   LIST(APPEND EXTRA_LIBS socket)
+   #MESSAGE("Using libsocket for socket functions")
+ENDIF(NOT LIBC_SOCKET)
+
+SET(TESTSRC "
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char **argv) {
+struct hostent *h = gethostbyname(argv[0]);
+return sizeof(h);
+}
+")
+
+CHECK_C_SOURCE_COMPILES("${TESTSRC}" LIBC_GETHOSTBYNAME)
+
+IF(NOT LIBC_SOCKET)
+   LIST(APPEND EXTRA_LIBS nls)
+   #MESSAGE("Using libsocket for socket functions")
+ENDIF(NOT LIBC_SOCKET)
+
