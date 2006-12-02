bd5eaa9f2eae297c0d5777367ab8f6fd1ccc8c37
blade <blade@a95a6be8-091b-0410-adaf-d31e6857962f>
2006-12-02 02:11:54 +0000

    Fallback to xnet if libnls does not work
    git-svn-id: svn://svn.debian.org/debburn/cdrkit/trunk@533 a95a6be8-091b-0410-adaf-d31e6857962f

--- include/AddNetworkBits.cmake
+++ include/AddNetworkBits.cmake
@@ -1,5 +1,7 @@
 
 # various checks add additional of extra libs, most likely for SunOS
+# using own checks and not CHECK_SYMBOLE because of spurios problems seen with
+# it
 
 INCLUDE(CheckCSourceCompiles)
 
@@ -13,12 +15,13 @@ return socket(AF_INET, SOCK_STREAM, 0);
 ")
 
 SET(CMAKE_REQUIRED_LIBRARIES )
-CHECK_C_SOURCE_COMPILES("${TESTSRC}" LIBC_SOCKET)
+CHECK_C_SOURCE_COMPILES("${TESTSRC}" USE_LIBC_SOCKET)
 
-IF(NOT LIBC_SOCKET)
+IF(NOT USE_LIBC_SOCKET)
    LIST(APPEND EXTRA_LIBS socket)
    #MESSAGE("Using libsocket for socket functions")
-ENDIF(NOT LIBC_SOCKET)
+   # enough testing. If it breaks, then it breaks, end of story
+ENDIF(NOT USE_LIBC_SOCKET)
 
 SET(TESTSRC "
 #include <sys/types.h>
@@ -31,10 +34,16 @@ return sizeof(h);
 }
 ")
 
-CHECK_C_SOURCE_COMPILES("${TESTSRC}" LIBC_NLS)
-
-IF(NOT LIBC_NLS)
-   LIST(APPEND EXTRA_LIBS nls)
-   #MESSAGE("Using libsocket for socket functions")
-ENDIF(NOT LIBC_NLS)
+CHECK_C_SOURCE_COMPILES("${TESTSRC}" USE_LIBC_NLS)
+IF(NOT USE_LIBC_NLS)
+   SET(CMAKE_REQUIRED_LIBRARIES nls)
+   CHECK_C_SOURCE_COMPILES("${TESTSRC}" USE_LIBNLS)
+   IF(USE_LIBNLS)
+      LIST(APPEND EXTRA_LIBS nls)
+   ELSE(USE_LIBNLS)
+      #      SET(CMAKE_REQUIRED_LIBRARIES xnet) # no more testing, enough
+      LIST(APPEND EXTRA_LIBS xnet)
+   ENDIF(USE_LIBNLS)
+ENDIF(NOT USE_LIBC_NLS)
+SET(CMAKE_REQUIRED_LIBRARIES )
 
